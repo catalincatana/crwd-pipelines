2e666064d1954c79fea767c57e7eb4e2bcb62184
Catalin Catana <catana@adobe.com>
2023-08-02 22:16:31 +0300

    wip - deployment pipelines

--- pipelines/deploy.groovy
+++ pipelines/deploy.groovy
@@ -11,7 +11,7 @@ pipeline {
         stage('Deploy') {
             steps {
                 // Mount the Kubernetes configuration file from credentials
-                withCredentials([file(credentialsId: kubeconfig, variable: 'KUBECONFIG')]) {
+                withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
                     container('kubectl') {
                         // Use the mounted kubeconfig file in the kubectl commands
                         script {
