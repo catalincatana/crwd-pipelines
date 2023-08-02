a82dc30330d6293ebbfa4ebaa06e02c736298821
Catalin Catana <catana@adobe.com>
2023-08-02 19:53:27 +0300

    first seed commit

--- pipelines/build.groovy
+++ pipelines/build.groovy
@@ -20,6 +20,7 @@ pipeline {
                 container('docker') {
                     script {
                         docker.build('catalincatana/crwd-app:latest')
+                        docker.tag('catalincatana/crwd-app:latest', 'catalincatana/crwd-repository:latest')
                     }
                 }
             }
@@ -29,7 +30,7 @@ pipeline {
                 container('docker') {
                     withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_CREDS', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
                         sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
-                        sh 'docker push catalincatana/crwd-repository:latest'
+                        sh 'docker push catalincatana/crwd-app:latest catalincatana/crwd-repository:latest'
                     }
                 }
             }
