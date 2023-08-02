5280cc2f4e3935853ecaec5c6f63e71fded79920
Catalin Catana <catana@adobe.com>
2023-08-02 23:42:03 +0300

    wip - deployment pipelines

--- pipelines/build.groovy
+++ pipelines/build.groovy
@@ -19,37 +19,38 @@ pipeline {
             steps {
                 container('docker') {
                     script {
-                        docker.build('catalincatana/crwd-repository:${TAG}')
+                        sh 'echo ${params.TAG}'
+                        docker.build('catalincatana/crwd-repository:${params.TAG}')
                     }
                 }
             }
         }
-        stage('Docker Push') {
-            steps {
-                container('docker') {
-                    withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_CREDS', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
-                        sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
-                        sh 'docker push catalincatana/crwd-repository:${TAG}'
-                    }
-                }
-            }
-        }
-        stage('Increment Version') {
-            steps {
-                // read VERSION file and increment the version
-                script {
-                    def version = readFile('VERSION').trim()
-                    def versionParts = version.split('\\.')
-                    def major = versionParts[0].toInteger()
-                    def minor = versionParts[1].toInteger()
-                    def patch = versionParts[2].toInteger()
-                    patch++
-                    def newVersion = "${major}.${minor}.${patch}"
-                    writeFile file: 'VERSION', text: newVersion
-                    echo "New version is ${newVersion}"
-                    git push origin master
-                }
-            }
-        }
+//        stage('Docker Push') {
+//            steps {
+//                container('docker') {
+//                    withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_CREDS', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+//                        sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+//                        sh 'docker push catalincatana/crwd-repository:${TAG}'
+//                    }
+//                }
+//            }
+//        }
+//        stage('Increment Version') {
+//            steps {
+//                // read VERSION file and increment the version
+//                script {
+//                    def version = readFile('VERSION').trim()
+//                    def versionParts = version.split('\\.')
+//                    def major = versionParts[0].toInteger()
+//                    def minor = versionParts[1].toInteger()
+//                    def patch = versionParts[2].toInteger()
+//                    patch++
+//                    def newVersion = "${major}.${minor}.${patch}"
+//                    writeFile file: 'VERSION', text: newVersion
+//                    echo "New version is ${newVersion}"
+//                    git push origin master
+//                }
+//            }
+//        }
     }
 }
