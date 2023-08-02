438e75493eea662bbceaf534ed6c0d2b1a0bb1b1
Catalin Catana <catana@adobe.com>
2023-08-02 19:33:29 +0300

    first seed commit

--- pipelines/build.groovy
+++ pipelines/build.groovy
@@ -2,37 +2,29 @@
 
 podTemplate(containers: [
         containerTemplate(
-                name: 'maven',
-                image: 'maven:3.8.1-jdk-8',
-                command: 'sleep',
-                args: '30d'
-        ),
-        containerTemplate(
-                name: 'python',
-                image: 'python:latest',
+                name: 'docker',
+                image: 'docker:latest',
                 command: 'sleep',
                 args: '30d')
 ]) {
 
     node(POD_LABEL) {
-        stage('Get a Maven project') {
-            git 'https://github.com/spring-projects/spring-petclinic.git'
-            container('maven') {
-                stage('Build a Maven project') {
-                    sh '''
-                    echo "maven build"
-                    '''
-                }
-            }
-        }
-
         stage('Get a Python Project') {
-            git url: 'https://github.com/hashicorp/terraform.git', branch: 'main'
-            container('python') {
-                stage('Build a Go project') {
-                    sh '''
-                    echo "Go Build"
-                    '''
+            container('docker') {
+                stage('Checkout') {
+                    steps {
+                        git branch: 'main',
+                                credentialsId: 'GITHUB_SSH_KEY',
+                                url: 'git@github.com:catalincatana/crwd-app.git'
+
+                    }
+                }
+                stage('Docker Build') {
+                    steps {
+                        script {
+                            docker.build('catalincatana/crwd-app:latest')
+                        }
+                    }
                 }
             }
         }
