8ba9c8e1b7194350afe17b9ace8be235b1d5643f
Catalin Catana <catana@adobe.com>
2023-08-02 19:48:07 +0300

    first seed commit

--- pipelines/build.groovy
+++ pipelines/build.groovy
@@ -13,7 +13,6 @@ pipeline {
                 git branch: 'main',
                         credentialsId: 'GITHUB_SSH_KEY',
                         url: 'git@github.com:catalincatana/crwd-app.git'
-
             }
         }
         stage('Docker Build') {
@@ -25,67 +24,15 @@ pipeline {
                 }
             }
         }
+        stage('Docker Push') {
+            steps {
+                container('docker') {
+                    withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_CREDS', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+                        sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+                        sh 'docker push catalincatana/crwd-app:latest'
+                    }
+                }
+            }
+        }
     }
 }
-
-//podTemplate(containers: [
-//        containerTemplate(
-//                name: 'docker',
-//                image: 'docker:latest',
-//                command: 'sleep',
-//                args: '30d')
-//]) {
-//
-//    node(POD_LABEL) {
-//        stage('Get a Python Project') {
-//            container('docker') {
-//                stage('Checkout') {
-//                    git branch: 'main',
-//                            credentialsId: 'GITHUB_SSH_KEY',
-//                            url: 'git@github.com:catalincatana/crwd-app.git'
-//
-//                }
-//                stage('Docker Build') {
-//                    script {
-//                        docker.build('catalincatana/crwd-app:latest')
-//                    }
-//                }
-//            }
-//        }
-//
-//    }
-//}
-
-
-
-
-//
-//pipeline {
-//    agent any
-//    stages {
-//        stage('Checkout') {
-//            steps {
-//                git branch: 'main',
-//                        credentialsId: 'GITHUB_SSH_KEY',
-//                        url: 'git@github.com:catalincatana/crwd-app.git'
-//
-//            }
-//        }
-//        stage('Docker Build') {
-//            steps {
-//                script {
-//                    docker.build('catalincatana/crwd-app:latest')
-//                }
-//            }
-//        }
-////        stage('Docker Push') {
-////            agent any
-////            steps {
-////                withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_CREDS', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
-////                    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
-////                    sh 'docker push catalincatana/crwd-app:latest'
-////                }
-////            }
-////        }
-//    }
-//}
