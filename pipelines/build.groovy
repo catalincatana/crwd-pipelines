bd2fb4487c56ca648b39bbaaea924b3c5402f885
Catalin Catana <catana@adobe.com>
2023-08-02 15:21:18 +0300

    first seed commit

---
+++ pipelines/build.groovy
@@ -0,0 +1,32 @@
+#!groovy
+
+pipeline {
+    agent none  stages {
+        stage('Checkout') {
+            agent any
+            steps {
+                checkout scm: scmGit(userRemoteConfigs: [
+                        [ credentialsId: 'GITHUB_SSH_KEY',
+                          url: 'git@github.com:catalincatana/crwd-app.git' ]
+                ])
+            }
+        }
+        stage('Docker Build') {
+            agent any
+            steps {
+                sh 'ls -la'
+                sh 'cd crwd-app'
+                sh 'docker build -t catalincatana/crwd-app:latest .'
+            }
+        }
+//        stage('Docker Push') {
+//            agent any
+//            steps {
+//                withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_CREDS', passwordVariable: 'dockerHubPassword', usernameVariable: 'dockerHubUser')]) {
+//                    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPassword}"
+//                    sh 'docker push catalincatana/crwd-app:latest'
+//                }
+//            }
+//        }
+    }
+}
--- seeds/crwdSeeds.groovy
+++ seeds/crwdSeeds.groovy
@@ -3,3 +3,26 @@ job('demo') {
         shell('echo Hello World!')
     }
 }
+
+pipelineJob('BuildCrwdApp') {
+    displayName('Build Crwd App')
+
+    parameters {
+        stringParam('TAG', '', 'Docker tag for the app')
+    }
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url ' git@github.com:catalincatana/crwd-pipelines.git'
+                        credentials('GITHUB_SSH_KEY')
+                    }
+                    branch('main')
+                }
+            }
+            scriptPath('pipelines/build.groovy')
+        }
+    }
+}
