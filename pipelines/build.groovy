1cad21759adedf113d57e5a0fbdfe2f5d7753c09
Catalin Catana <catana@adobe.com>
2023-08-03 00:15:39 +0300

    wip - deployment pipelines

--- pipelines/build.groovy
+++ pipelines/build.groovy
@@ -35,22 +35,5 @@ pipeline {
                 }
             }
         }
-        stage('Increment Version') {
-            steps {
-                // read VERSION file and increment the version
-                script {
-                    def version = readFile('VERSION').trim()
-                    def versionParts = version.split('\\.')
-                    def major = versionParts[0].toInteger()
-                    def minor = versionParts[1].toInteger()
-                    def patch = versionParts[2].toInteger()
-                    patch++
-                    def newVersion = "${major}.${minor}.${patch}"
-                    writeFile file: 'VERSION', text: newVersion
-                    echo "New version is ${newVersion}"
-                    git push origin master
-                }
-            }
-        }
     }
 }
